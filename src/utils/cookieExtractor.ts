cbfc02b456c4f0ff1332d1976c70699e8fc4f975
Luca Améaume <62576432+LuluxClient@users.noreply.github.com>
2024-12-22 22:55:04 +0100

    more work uwu

--- src/utils/cookieExtractor.ts
+++ src/utils/cookieExtractor.ts
@@ -2,8 +2,14 @@ import fs from 'fs/promises';
 
 export async function extractYoutubeCookies() {
     try {
-        // Créer un fichier de cookies basique avec le consentement
-        const cookieContent = `youtube.com	TRUE	/	TRUE	2597573456	CONSENT	YES+`;
+        // Créer un fichier de cookies au format Netscape
+        const cookieContent = `# Netscape HTTP Cookie File
+# https://curl.haxx.se/rfc/cookie_spec.html
+# This is a generated file!  Do not edit.
+
+youtube.com	TRUE	/	TRUE	2597573456	CONSENT	YES+
+.youtube.com	TRUE	/	TRUE	2597573456	PREF	tz=Europe.Paris
+`;
         
         await fs.writeFile('/root/cookies.txt', cookieContent);
         console.log('Fichier de cookies YouTube créé avec succès');
